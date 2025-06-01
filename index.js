651f82a1c1740b98686701bd58aabd0024d0d3c3
Bishal Bhandari <bhandarisuraksha617@gmail.com>
2025-06-01 21:29:15 +0545

    async wrap function added

--- index.js
+++ index.js
@@ -27,101 +27,92 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
+//  Error-Handling for Asynch Function (Note : In express v5  , no  need to do this , as express has the feature of it automatically)
+function asyncWrap(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    }
+}
 
 //show all data
-app.get("/chats", async (req, res, next) => {
-    try {
-        let chats = await Chat.find();
-        res.render("index.ejs", { chats });
-    } catch (err) {
-        next(err);
-    }
-});
+app.get("/chats", asyncWrap(async (req, res, next) => {
+
+    let chats = await Chat.find();
+    res.render("index.ejs", { chats });
+
+}));
 
 //create a new chat
 app.get("/chats/new", (req, res) => {
     res.render("new.ejs");
 })
 
-app.post("/chats", async (req, res, next) => {
-    try {
-        let { from, msg, to } = req.body;
-        let chat = new Chat({
-            from: from,
-            to: to,
-            msg: msg,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        });
-        await chat.save();
-        res.redirect("/chats");
-    } catch (err) {
-        next(err);
-    };
-});
+app.post("/chats", asyncWrap(async (req, res, next) => {
+
+    let { from, msg, to } = req.body;
+    let chat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    });
+    await chat.save();
+    res.redirect("/chats");
+
+}));
+
+
 
 
 // New - Show Routes
-app.get("/chats/:id", async (req, res, next) => {
-    try {
-        let { id } = req.params;
-        let chat = await Chat.findById(id);
-        if (!chat) {
-            console.log("yes entered");
-            return next(new ExpressError(404, "Chat Not Found!"))
-        }
-        res.render("edit.ejs", { chat });
-    } catch (err) {
-        next(err);
+app.get("/chats/:id", asyncWrap(async (req, res, next) => {
+
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        console.log("yes entered");
+        return next(new ExpressError(404, "Chat Not Found!"))
     }
-});
+    res.render("edit.ejs", { chat });
+}));
 
 
 // Edit a Chat
-app.get("/chats/:id/edit", async (req, res, next) => {
-    try {
-        let { id } = req.params;
-        let chat = await Chat.findById(id);
-        res.render("edit.ejs", { chat });
-    } catch (err) {
-        next(err);
-    }
+app.get("/chats/:id/edit", asyncWrap(async (req, res, next) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
 
-});
+}));
 
 
 // Update Route
-app.put("/chats/:id", async (req, res, next) => {
-    try {
-        let { id } = req.params;
-        let { msg: newMsg } = req.body;
-        let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true, new: true });
-        let latestUpdate = await Chat.findByIdAndUpdate(id, { updatedAt: new Date() }, { runValidators: true, new: true });
-        res.redirect("/chats");
-    } catch (err) {
-        next(err);
-    }
-
-});
+app.put("/chats/:id", asyncWrap(async (req, res, next) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true, new: true });
+    let latestUpdate = await Chat.findByIdAndUpdate(id, { updatedAt: new Date() }, { runValidators: true, new: true });
+    res.redirect("/chats");
+}));
 
 
 
 // Delete Chat
-app.delete("/chats/:id", async (req, res, next) => {
-    try {
-        let { id } = req.params;
-        let chat = await Chat.findByIdAndDelete(id);
-        console.log(chat);
-        res.redirect("/chats");
-    } catch (err) {
-        next(err);
-    }
-});
+app.delete("/chats/:id", asyncWrap(async (req, res, next) => {
+
+    let { id } = req.params;
+    let chat = await Chat.findByIdAndDelete(id);
+    console.log(chat);
+    res.redirect("/chats");
+
+}));
 
 
 app.get("/", (req, res) => {
     res.send("Home Page!");
 });
+
 
 
 
